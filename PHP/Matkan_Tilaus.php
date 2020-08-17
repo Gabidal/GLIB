c93190127ad1b41534c69f67b5fb4f171c483295
Gabidal <Quanfoxes@gmail.com>
2020-08-17 16:39:59 +0200

    added php

--- PHP/Matkan_Tilaus.php
+++ PHP/Matkan_Tilaus.php
@@ -1,4 +1,16 @@
+
+
+<!doctype html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../CSS/style.css"/>
+</head>
+<body>
+</body>
+
+</html>
 <?php
+
     $source_country = isset($_POST['source_country']) ? $_POST['source_country'] : '';
     $dest_country = isset($_POST['dest_country']) ? $_POST['dest_country'] : '';
     $Parent_Count = isset($_POST['Parent_Count']) ? $_POST['Parent_Count'] : '';
@@ -11,13 +23,17 @@
 
     //tähän tulee sähkö postin osoitteen otto
     //joku jolla on hyvä tyyli maku tuu tekee tästä parempaa :D.
-    echo "<form method=\"post\" action=\"\">";
-    echo "<input type=\"text\" name=\"value\" placeholder=\"Sähköpostinne\">";
-    echo "<input type=\"submit\">";
-    echo "</form>";
+    echo "<form class=\"flat\" method=\"post\" action=\"\">";
+    echo "<input class=\"glass\" type=\"text\" name=\"value\" placeholder=\"Sähköpostinne\">";
+    echo "<input class=\"glass-button\" type=\"submit\">";
 
     $Mail_address =  isset($_POST['value']) ? $_POST['value'] : '';
+    $content = "Lento lippu varattu: $date_of_departure\nPaluu päivä: $return_date\nLähtö maa:$source_country\nPäämäärä:$dest_country\nGrown-up amount:$Parent_Count\nChildren amount:$Child_count";
 
-    echo "Lippu lähetetty osoitteeseen: ". $Mail_address;
-    
+    mail($Mail_address, "Lento lipunne olkaa hyvä!", $content);
+    echo "<p class=\"glass-text\">Lippu lähetetty osoitteeseen: </p>". "<p class=\"glass-text\"> $Mail_address </p>";
+
+    echo "</form>";
+
+
 ?>
