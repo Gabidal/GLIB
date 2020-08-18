560f6fffec2e369dc8615cab91bafb31e56044c4
Gabidal <Quanfoxes@gmail.com>
2020-08-18 12:28:42 +0200

    test1

--- PHP/Matkan_Tilaus.php
+++ PHP/Matkan_Tilaus.php
@@ -15,25 +15,28 @@
     $dest_country = isset($_POST['dest_country']) ? $_POST['dest_country'] : '';
     $Parent_Count = isset($_POST['Parent_Count']) ? $_POST['Parent_Count'] : '';
     $Child_count = isset($_POST['Child_count']) ? $_POST['Child_count'] : '';
-    $date_of_departure = isset($_POST['date_of_departure']) ? $_POST['date_of_departure'] : '';
-    $return_date = isset($_POST['return_date']) ? $_POST['return_date'] : '';
+    $date_of_departure = new DateTime(isset($_POST['date_of_departure']) ? $_POST['date_of_departure'] : '');
+    $return_date = new DateTime(isset($_POST['return_date']) ? $_POST['return_date'] : '');
+    $Mail_address =  isset($_POST['value']) ? $_POST['value'] : '';
+
+    $content = "Lento lippu varattu: ". $date_of_departure->format('d.m.Y'). lentomies(). "\nPaluu päivä: ". $return_date->format('d.m.Y'). lentomies().  "\nLähtö maa:". $source_country. "\nPäämäärä:". $dest_country. "\nAikuisten määrä:". $Parent_Count. "\nLasten määrä: ". $Child_count;
 
     //nopea testi että saako kaikki variablet oikeat arvot :D
     //echo $source_country. $dest_country. $Parent_Count. $Child_count. $date_of_departure. $return_date
 
     //tähän tulee sähköpostin osoitteenotto
     //joku jolla on hyvä tyyli maku tuu tekee tästä parempaa :D.
-    echo "<form class=\"flat\" method=\"post\" action=\"\">";
-    echo "<input class=\"glass\" type=\"text\" name=\"value\" placeholder=\"Sähköpostinne\">";
-    echo "<input class=\"glass-button\" type=\"submit\">";
 
-    $Mail_address =  isset($_POST['value']) ? $_POST['value'] : '';
-    $content = "Lento lippu varattu: $date_of_departure\nPaluu päivä: $return_date\nLähtö maa:$source_country\nPäämäärä:$dest_country\nGrown-up amount:$Parent_Count\nChildren amount:$Child_count";
+    echo $content;
 
     mail($Mail_address, "Lento lipunne olkaa hyvä!", $content);
     echo "<p class=\"glass-text\">Lippu lähetetty osoitteeseen: </p>". "<p class=\"glass-text\"> $Mail_address </p>";
 
     echo "</form>";
 
+
+    function lentomies(){
+        return " ". rand(7, 22). ":". rand(0, 3) * 15;
+    }
 
 ?>
